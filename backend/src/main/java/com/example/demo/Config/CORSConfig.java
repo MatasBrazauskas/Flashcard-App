90e9f3378134aae7583bceae96164c60f3c200b1
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2025-08-03 22:54:20 +0300

    Clean up

--- backend/src/main/java/com/example/demo/Config/CORSConfig.java
+++ backend/src/main/java/com/example/demo/Config/CORSConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all paths ("/**") in your API
-                .allowedOrigins(allowedOrigins, "http://localhost:5173")
-                .allowedMethods("*") // Allowed HTTP methods
+                .allowedOrigins(
+                        "http://localhost:5173", // The common port for Vite
+                        "http://localhost:3000"  // The common port for create-react-app
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allowed headers for the actual request
                 .allowCredentials(true) // Allow sending of cookies, authorization headers, etc.
                 .maxAge(3600); // How long the preflight request (OPTIONS) can be cached in seconds
